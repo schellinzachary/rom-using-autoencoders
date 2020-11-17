eba12829fee986f3da3a781d1ecd1345ac949de7
haengendeuhrzeiger <zachary.schellin@campus.tu-berlin.de>
2020-11-17 20:55:42 +0100

    Convolution NN build. Batchsize evaluated. Leaves learningrate and other hyperparameters, as well as inference check. Until now a test error of 3e-4 is acieved

---
+++ Neural_Network/Conv_Nets/Conv_AE_1_0.py
@@ -0,0 +1,228 @@
+'''
+Convolutional Autoencoder v1.0
+'''
+
+
+import numpy as np
+import torch
+import torch.nn as nn
+from torch.optim import Adam, SGD, Adadelta, Adagrad
+import torch.tensor as tensor
+import matplotlib.pyplot as plt
+import scipy.io as sio
+from torch.utils.data import DataLoader
+
+torch.cuda.empty_cache()
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+print(torch.cuda.is_available())
+
+N_EPOCHS = 1000
+BATCH_SIZE = 4
+lr = 1e-3
+
+device = 'cpu'
+
+#load data
+f = np.load('/home/zachi/Documents/ROM_using_Autoencoders/Neural_Network/preprocessed_samples_conv.npy')
+
+#shuffe or not to shuffle ----> k-means???
+#np.random.shuffle(f)
+f = tensor(f, dtype=torch.float).to(device)
+
+train_in = f[0:31]
+val_in = f[32:39]
+
+
+train_iterator = DataLoader(train_in, batch_size = BATCH_SIZE)
+test_iterator = DataLoader(val_in, batch_size = int(len(f)*0.2))
+print('test_it',len(test_iterator),'train_it',len(train_iterator))
+
+
+N_TRAIN_STEPS = 2000
+
+
+
+class Encoder(nn.Module):
+    def __init__(self):
+        super(Encoder, self).__init__()
+        self.convE1 = nn.Conv2d(1,8,(3,2),stride=2)
+        self.convE2 = nn.Conv2d(8,16,(2,2),stride=2)
+        self.convE3 = nn.Conv2d(16,32,(2,2),stride=2)
+        self.convE4 = nn.Conv2d(32,64,(3,3),stride=(1,2))
+        self.linearE1 = nn.Linear(in_features=768,out_features=3)
+        self.act = nn.Tanh()
+
+    def forward(self, x):
+        x = self.act(self.convE1(x))
+        x = self.act(self.convE2(x))
+        x = self.act(self.convE3(x))
+        x = self.act(self.convE4(x))
+        original_size = x.size()
+        x = x.view(original_size[0],-1)
+        x = self.linearE1(x)
+        return x
+
+
+class Decoder(nn.Module):
+    def __init__(self):
+        super(Decoder, self).__init__()
+        self.linearD1 = nn.Linear(in_features=3, out_features=768)
+        self.convD1 = nn.ConvTranspose2d(64,32,(3,3),stride=(1,2))
+        self.convD2 = nn.ConvTranspose2d(32,16,(2,2),stride=2)
+        self.convD3 = nn.ConvTranspose2d(16,8,(2,2),stride=2)
+        self.convD4 = nn.ConvTranspose2d(8,1,(3,2),stride=2)
+        self.act = nn.Tanh()
+
+    def forward(self, x):
+        x = self.linearD1(x)
+        dim = x.shape[0]
+        x = torch.reshape(x,[dim,64,1,12])
+        x = self.act(self.convD1(x))
+        x = self.act(self.convD2(x))
+        x = self.act(self.convD3(x))
+        x = self.act(self.convD4(x))
+        return x
+
+class Autoencoder(nn.Module):
+    def __init__(self, enc, dec):
+        super(Autoencoder, self).__init__()
+        self.encoder = Encoder()
+        self.decoder = Decoder()
+
+    def forward(self, x):
+        x = self.encoder(x)
+        x = self.decoder(x)
+        return x
+
+class Swish(nn.Module):
+    def forward(self, x):
+        return x * torch.sigmoid(x)
+
+#encoder
+encoder = Encoder()
+
+#decoder
+decoder = Decoder()
+
+#Autoencoder
+model = Autoencoder(encoder, decoder).to(device)
+
+optimizer = Adam(params=model.parameters(), lr=lr)
+
+loss_crit = nn.MSELoss()
+train_losses = []
+val_losses = []
+
+
+def train():
+
+    model.train()
+
+    train_loss = 0.0
+
+    for batch_ndx, x in enumerate(train_iterator):
+
+        x = x.to(device)
+
+        optimizer.zero_grad()
+
+        predicted = model(x)
+
+        loss = loss_crit(predicted,x)
+
+        loss.backward()
+        train_loss += loss.item()
+
+        optimizer.step()
+
+    return train_loss
+def test():
+
+    model.eval()
+
+    test_loss = 0
+
+    with torch.no_grad():
+        for i, x in enumerate(test_iterator):
+
+            x = x.to(device)
+
+            predicted = model(x)
+
+            loss = loss_crit(predicted,x)
+            test_loss += loss.item()
+
+        return test_loss
+
+test_losses = []
+val_losses = []
+
+#checkpoint Load
+# checkpoint = torch.load('Lin_AE_STATE_DICT_0_9_L5_substr50_test.pt')
+# model.load_state_dict(checkpoint['model_state_dict'])
+# #optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+# epoch_o = checkpoint['epoch']
+# train_loss = checkpoint['train_loss']
+# test_loss = checkpoint['test_loss']
+# train_losses = checkpoint['train_losses']
+# test_losses = checkpoint['test_losses']
+
+
+for epoch in range(N_EPOCHS):
+
+    train_loss = train()
+    test_loss = test()
+
+    #save and print the loss
+    train_loss /= len(train_iterator)
+    test_loss /= len(test_iterator)
+    
+    train_losses.append(train_loss)
+    test_losses.append(test_loss)
+
+    print(f'Epoch {epoch}, Train Loss: {train_loss:.10f}, Test Loss: {test_loss:.10f}')
+
+
+    # if epoch % 100 == 0:
+
+    #     i = randint(0,999)
+    #     x = val_in[i].to(device)
+
+    #     predicted = model(x)
+    #     x = x.to('cpu')
+    #     predicted = predicted.to('cpu')
+    #     data = x.detach().numpy()
+    #     predict = predicted.detach().numpy()
+        
+    #     plt.plot(x, label='Original')
+    #     plt.plot(predict, label='Predicted')
+    #     plt.legend()
+    #     plt.show()
+
+plt.figure()
+plt.semilogy(np.arange(N_EPOCHS), train_losses, label='Training loss')
+plt.semilogy(np.arange(N_EPOCHS), test_losses, label='Test loss')
+plt.legend(loc='upper right')
+plt.xlabel('trainstep')
+plt.ylabel('loss')
+plt.show()
+
+
+# np.save('Train_Loss_Lin_0_9_L5_substr50_test.npy',train_losses)
+# np.save('Test_Loss_Lin_0_9_L5_substr50_test.npy',test_losses)
+
+
+
+
+#save the models state dictionary for inference
+# torch.save({
+#     'epoch': epoch,
+#     'model_state_dict':model.state_dict(),
+#     'optimizer_state_dict': optimizer.state_dict(),
+#     'train_loss': train_loss,
+#     'test_loss': test_loss,
+#     'train_losses':train_losses,
+#     'test_losses': test_losses
+#     },'Conv_AE_STATE_DICT_0_9_L3_Tanh.pt')
--- Neural_Network/Preprocessing_Conv.py
+++ Neural_Network/Preprocessing_Conv.py
@@ -7,52 +7,49 @@ import scipy.io as sio
 import numpy as np
 
 
-data_set = sio.loadmat('/home/zachary/Desktop/BA/data_sod/sod25Kn0p01/f.mat')
+data_set = sio.loadmat('/home/zachi/Documents/ROM_using_Autoencoders/data_sod/sod25Kn0p01/f.mat')
 
 
 dataset  = data_set['f']
-#dataset = np.array(dataset,order='C')
+print(dataset.T.shape)
+
 dataset = np.reshape(dataset,(40,25,200),order='C')
 
-
-plt.imshow(dataset[1,:,:])
-plt.xlabel('x')
-plt.ylabel('t')
-plt.colorbar()
-plt.show()
 
 
 #Normalizing the Input
 
-dataset = (dataset - np.min(dataset)) / ( np.max(dataset) - np.min(dataset) )
+#dataset = (dataset - np.min(dataset)) / ( np.max(dataset) - np.min(dataset) )
 
 
-#dataset = np.expand_dims(dataset, axis=1)
+dataset = np.expand_dims(dataset, axis=1)
+np.random.shuffle(dataset)
 
-#np.save('preprocessed_samples_conv',dataset)
+
+np.save('preprocessed_samples_conv',dataset)
 
 
 
 
 
 #First set up the figure, the axis, and the plot element we want to animate
-fig = plt.figure( )
+# fig = plt.figure( )
 
-a = dataset[0]
-im = plt.imshow(a)
+# a = dataset[0]
+# im = plt.imshow(a)
 
-def animate_func(i):
+# def animate_func(i):
 
-    im.set_array(dataset[i])
-    return [im]
+#     im.set_array(dataset[i])
+#     return [im]
 
-anim = animation.FuncAnimation(
-                               fig, 
-                               animate_func, 
-                               #frames = nSeconds * fps,
-                               #interval = 1000 / fps, # in ms
-                               )
+# anim = animation.FuncAnimation(
+#                                fig, 
+#                                animate_func, 
+#                                #frames = nSeconds * fps,
+#                                #interval = 1000 / fps, # in ms
+#                                )
 
 
-plt.show()
+# plt.show()
 
