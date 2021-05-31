4fc06205fc48590fc0cfad428a4a8ba7cde28c47
haengendeuhrzeiger <zachary.schellin@camput.tu-berlin.de>
2021-05-31 21:12:38 +0200

    update conv appendix

--- 04_Autoencoder/02_Convolutional/Parameterstudy/Results_layer/Results_Layer.py
+++ 04_Autoencoder/02_Convolutional/Parameterstudy/Results_layer/Results_Layer.py
@@ -6,6 +6,8 @@ from pathlib import Path
 from os.path import join
 home = str(Path.home())
 loc_data = "rom-using-autoencoders/04_Autoencoder/Preprocessing/Data/flow_4D.npy"
+loc_plot = "rom-using-autoencoders/01_Thesis/Figures/Parameterstudy/Convolutional/Layer.tex"
+
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -26,15 +28,6 @@ from torch.optim.lr_scheduler import MultiStepLR
 torch.manual_seed(42)
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-#Scaler
-class TorchStandardScaler:
-  def fit(self, x):
-    self.mean = x.mean(0, keepdim=True)
-    self.std = x.std(0, unbiased=False, keepdim=True)
-  def transform(self, x):
-    x -= self.mean
-    x /= (self.std)
-    return x
 
 #load data
 f = np.load(join(home,loc_data))
@@ -48,9 +41,15 @@ f = tensor(f, dtype=torch.float).to(device)
 class Encoder_2(nn.Module):
     def __init__(self):
         super(Encoder_2, self).__init__()
-        self.convE1 = nn.Conv2d(1,16,(5,5),stride=(5,5))
-        self.convE2 = nn.Conv2d(16,32,(5,5),stride=(5,5))
-        self.linearE1 = nn.Linear(in_features=256,out_features=5)
+        self.convE1 = nn.Conv2d(
+            1,8,(5,5),
+            stride=(5,5)
+            )
+        self.convE2 = nn.Conv2d(
+            8,16,(5,5),
+            stride=(5,5)
+            )
+        self.linearE1 = nn.Linear(in_features=128,out_features=5)
         self.add_module('act',nn.ReLU())
 
 
@@ -67,14 +66,14 @@ class Decoder_2(nn.Module):
         super(Decoder_2, self).__init__()
         self.linearD1 = nn.Linear(
             in_features=5,
-            out_features=256
+            out_features=128
             )
         self.convD1 = nn.ConvTranspose2d(
-            32,16,(5,5),
+            16,8,(5,5),
             stride=(5,5)
             )
         self.convD2 = nn.ConvTranspose2d(
-            16,1,(5,5),
+            8,1,(5,5),
             stride=(5,5)
             )
         self.add_module('act',nn.ReLU())
@@ -83,7 +82,7 @@ class Decoder_2(nn.Module):
     def forward(self, x):
         x = self.linearD1(x)
         dim = x.shape[0]
-        x = torch.reshape(x,[dim,32,1,8])
+        x = torch.reshape(x,[dim,16,1,8])
         x = self.act(self.convD1(x))
         x = self.act(self.convD2(x))
         return x
@@ -92,21 +91,21 @@ class Encoder_3(nn.Module):
     def __init__(self):
         super(Encoder_3, self).__init__()
         self.convE1 = nn.Conv2d(
-            1,8,(3,3),
+            1,4,(3,3),
             stride=(3,3),
             padding=(1,1)
             )
         self.convE2 = nn.Conv2d(
-            8,16,(3,3),
+            4,8,(3,3),
             stride=(3,3),
             padding=(0,1)
             )
         self.convE3 = nn.Conv2d(
-            16,32,(3,3),
+            8,16,(3,3),
             stride=(3,3),
             padding=(0,1)
             )
-        self.linearE1 = nn.Linear(in_features=256,
+        self.linearE1 = nn.Linear(in_features=128,
             out_features=5)
         self.add_module('act',nn.ReLU())
 
@@ -124,22 +123,22 @@ class Decoder_3(nn.Module):
     def __init__(self):
         super(Decoder_3, self).__init__()
         self.linearD1 = nn.Linear(in_features=5,
-            out_features=256
+            out_features=128
             )
         self.convD1 = nn.ConvTranspose2d(
-            32,16,(3,3),
-            stride=(3,3),
-            padding=(0,1),
-            output_padding=(0,1)
-            )
-        self.convD2 = nn.ConvTranspose2d(
             16,8,(3,3),
             stride=(3,3),
             padding=(0,1),
             output_padding=(0,1)
             )
+        self.convD2 = nn.ConvTranspose2d(
+            8,4,(3,3),
+            stride=(3,3),
+            padding=(0,1),
+            output_padding=(0,1)
+            )
         self.convD3 = nn.ConvTranspose2d(
-            8,1,(3,3),
+            4,1,(3,3),
             stride=(3,3),
             padding=(1,2)
             )
@@ -149,7 +148,7 @@ class Decoder_3(nn.Module):
     def forward(self, x):
         x = self.linearD1(x)
         dim = x.shape[0]
-        x = torch.reshape(x,[dim,32,1,8])
+        x = torch.reshape(x,[dim,16,1,8])
         x = self.act(self.convD1(x))
         x = self.act(self.convD2(x))
         x = self.convD3(x)
@@ -159,26 +158,26 @@ class Encoder_4(nn.Module):
     def __init__(self):
         super(Encoder_4, self).__init__()
         self.convE1 = nn.Conv2d(
-            1,8,(3,3),
+            1,2,(3,3),
             stride=(3,3),
             padding=(3,2)
             )
         self.convE2 = nn.Conv2d(
-            8,16,(3,3),
+            2,4,(3,3),
             stride=(3,3),
             padding=(3,2),
             )
         self.convE3 = nn.Conv2d(
-            16,32,(3,3),
+            4,8,(3,3),
             stride=(3,3),
             padding=(3,2)
             )
         self.convE4 = nn.Conv2d(
-            32,64,(3,3),
+            8,16,(3,3),
             stride=(3,3),
             padding=(0,0)
             )
-        self.linearE1 = nn.Linear(in_features=192,
+        self.linearE1 = nn.Linear(in_features=48,
             out_features=5
             )
         self.add_module('act',nn.ReLU())
@@ -198,28 +197,28 @@ class Decoder_4(nn.Module):
     def __init__(self):
         super(Decoder_4, self).__init__()
         self.linearD1 = nn.Linear(in_features=5,
-        out_features=192
+        out_features=48
         )
         self.convD1 = nn.ConvTranspose2d(
-            64,32,(3,3),
+            16,8,(3,3),
             stride=(3,3),
             padding=(0,0),
             output_padding=(0,0)
             )
         self.convD2 = nn.ConvTranspose2d(
-            32,16,(3,3),
+            8,4,(3,3),
             stride=(3,3),
             padding=(2,2),
             output_padding=(0,1)
             )
         self.convD3 = nn.ConvTranspose2d(
-            16,8,(3,3),
+            4,2,(3,3),
             stride=(3,3),
             padding=(3,2),
             output_padding=(0,0)
             )
         self.convD4 = nn.ConvTranspose2d(
-            8,1,(3,3),
+            2,1,(3,3),
             stride=(3,3),
             padding=(1,2)
             )
@@ -229,11 +228,16 @@ class Decoder_4(nn.Module):
     def forward(self, x):
         x = self.linearD1(x)
         dim = x.shape[0]
-        x = torch.reshape(x,[dim,64,1,3])
+        x = torch.reshape(x,[dim,16,1,3])
+        print(x.shape)
         x = self.act(self.convD1(x))
+        print(x.shape)
         x = self.act(self.convD2(x))
+        print(x.shape)
         x = self.act(self.convD3(x))
+        print(x.shape)
         x = self.convD4(x)
+        print(x.shape)
         return x
 
 class Autoencoder(nn.Module):
@@ -248,12 +252,12 @@ class Autoencoder(nn.Module):
         return x
 
 enc_dict = [
-    Encoder_2(),
+    #Encoder_2(),
     Encoder_3(),
     Encoder_4()
 ]
 dec_dict = [
-    Decoder_2(),
+    #Decoder_2(),
     Decoder_3(),
     Decoder_4()
 ]
@@ -261,14 +265,14 @@ dec_dict = [
 best_models = (
     # "model3-epoch1399-val_loss7.762E-03-exp-1",
     # "model4-epoch1094-val_loss1.474E-01-exp-1",
-    # "model3-epoch1975-val_loss1.537E-05-exp-2",
-    # "model4-epoch1924-val_loss8.178E-04-exp-2"
+    "model3-epoch1975-val_loss1.537E-05-exp-2",
+    "model4-epoch1924-val_loss8.178E-04-exp-2"
     # "model2-epoch1969-val_loss8.009E-06-exp-3",
     # "model3-epoch1980-val_loss1.326E-05-exp-3",
     # "model4-epoch109-val_loss6.976E-03-exp-3"
-    "model2-epoch1-val_loss7.619E-03-exp-4",
-    "model3-epoch1944-val_loss1.068E-05-exp-4",
-    "model4-epoch1969-val_loss9.496E-06-exp-4"
+    # "model2-epoch1-val_loss7.619E-03-exp-4",
+    # "model3-epoch1944-val_loss1.068E-05-exp-4",
+    # "model4-epoch1969-val_loss9.496E-06-exp-4"
     )
 
 train_losses = []
@@ -277,7 +281,9 @@ l2_losses = []
 variable = []
 min_idx = []
 
-fig, ax = plt.subplots(3,1)
+
+
+fig, ax = plt.subplots(1,2)
 
 for idx, (encoder, decoder) in enumerate(zip(enc_dict,dec_dict)):
 
@@ -290,7 +296,7 @@ for idx, (encoder, decoder) in enumerate(zip(enc_dict,dec_dict)):
     model = Autoencoder(encoder, decoder).to(device)
 
     checkpoint_model = torch.load('{}.pt'.format(best_model))
-    checkpoint_loss = torch.load('last-model-{}exp-4.pt'.format(idx+2))
+    checkpoint_loss = torch.load('last-model-{}exp-2.pt'.format(idx+3))
 
     model.load_state_dict(checkpoint_model['model_state_dict'])
     train_loss = checkpoint_loss['train_losses']
@@ -313,11 +319,11 @@ for idx, (encoder, decoder) in enumerate(zip(enc_dict,dec_dict)):
     ax[idx].semilogy(val_loss,'k''-',label='Test')
     ax[idx].set_xlabel('Epoch')
     ax[idx].set_ylabel('MSE Loss')
-    ax[idx].set_title('fold{} '.format(idx))
+    ax[idx].set_title('Model{} '.format(idx+3))
     ax[idx].set_ylim(ymax=1e-2)
     ax[idx].legend()
 
-#tikzplotlib.save(join(home,loc_plot))
+tikzplotlib.save(join(home,loc_plot))
 
 
 loss_dict = {
